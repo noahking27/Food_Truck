49b58011d52f65ac7cb01f9334507c2ec8771c77
Brian Manchester <brianmanchester@brians-imac.attlocal.net>
2017-06-09 20:37:32 -0400

    routing and database set up

---
+++ models/trucks.js
@@ -0,0 +1,57 @@
+module.exports = function(sequelize, DataTypes) {
+	var Foodtrucks = sequelize.define("Foodtrucks", {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true
+			}
+		},
+		food_type: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true
+			}
+		},
+		popular_item: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			defaultValue: null,
+			validate: {
+				notEmpty: true
+			}
+		},
+		website: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			defaultValue: null,
+			validate: {
+				isUrl: true
+			}
+		},
+		twitter_handle: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			defaultValue: null,
+			validate: {
+				notEmpty: true
+			}
+		},
+		created_at: {
+			type: DataTypes.DATEONLY,
+			allowNull: false,
+			defaultValue: DataTypes.NOW
+		}
+	}, {
+		timestamps: false,
+		classMethods: {
+			associate: function(models) {
+				Foodtrucks.hasMany(models.Reviews, {
+					onDelete: "cascade"
+				});
+			}
+		}
+	});
+	return Foodtrucks;
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var htmlRoutes = require("./controllers/html_routes.js");
+var apiRoutes = require("./controllers/api_routes.js");
+var app = express();
+var PORT = process.env.PORT || 3030;
+var db = require("./models");
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", htmlRoutes);
+
+app.use("/api/", apiRoutes);
+
+db.sequelize.sync().then(function() {
+	app.listen(PORT, console.log("Listening on port: " + PORT));
+});
